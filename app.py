163e326136115e8bdefa5099c535ab43f4d50bd1
Shriya Haral <67820562+shriyaharal7@users.noreply.github.com>
2021-08-20 20:33:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+# import statements
+import streamlit as st
+import numpy as np
+import pandas as pd
+# adding title and text in the app
+st.title('Hello World')
+st.write("Congratulations! You have deployed your first web application.")
